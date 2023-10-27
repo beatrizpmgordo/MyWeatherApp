64cfc5228c12d42f0cfc50015238b346991d7bb0
beatrizpmgordo <beatriz.gordo@hotmail.com>
2023-10-27 13:12:47 +0100

    Beggining the development of the JavaScript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+function displayTemperature(response) {
+  console.log(response);
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.condition.description;
+}
 
+let apiKey = "f388b055e9fa4ta275o3155340404ccb";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
